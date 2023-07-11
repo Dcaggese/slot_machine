7049282bce25e79af355953a74819e18c2914d59
Dominic Caggese <dcaggese@live.com>
2023-07-11 01:11:19 -0400

    deposit function written

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 
+#ask the user for a deposit of money
 def deposit():
-    
+    while True:
+        amount = input("How much would you like to deposit? $")
+        #check for valid whole number and convert string to int
+        if amount.isdigit():
+            amount = int(amount)
+            #exit loop and return amount if greater than 0
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a valid amount of money.")
+
+    return amount
+
+def main():
+    balance = deposit()
+
+main()
